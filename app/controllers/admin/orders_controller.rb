e3ff11a4fb1762df5738b8bfa4abf803be76f69f
EC2 Default User <ec2-user@ip-172-31-13-179.ap-northeast-1.compute.internal>
2022-06-08 14:57:01 +0000

    [Complete]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,16 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(params[:id])
-    
+    @order_detail=OrderDetail.find(params[:id])
+  end
+
+  def update
+    @order=Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path
+  end
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
